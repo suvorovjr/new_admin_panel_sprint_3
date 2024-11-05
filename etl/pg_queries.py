6aac154f4e26ef5d1b0ddcbce605863c7dabaa13
suvorovjr <pglavan1998@mail.ru>
2024-11-05 11:11:30 +0300

    Add changes in queries

--- etl/pg_queries.py
+++ etl/pg_queries.py
@@ -1,54 +1,46 @@
 from psycopg import sql
 
+# Запрос для получения фильмов и связанных данных по переданным идентификаторам
 get_film_works_query = sql.SQL("""
 SELECT 
-    fw.id AS id,
-    fw.title AS title,
-    fw.description AS description,
+    fw.id,
+    fw.title,
+    fw.description,
     fw.rating AS imdb_rating,
-    ARRAY(SELECT g.name FROM content.genre AS g
-          JOIN content.genre_film_work AS gfw ON g.id = gfw.genre_id
-          WHERE gfw.film_work_id = fw.id) AS genres,
-    ARRAY(SELECT p.full_name FROM content.person AS p
-          JOIN content.person_film_work AS pfw ON p.id = pfw.person_id
-          WHERE pfw.film_work_id = fw.id AND pfw.role = 'DR') AS directors_names,
-    ARRAY(SELECT p.full_name FROM content.person AS p
-          JOIN content.person_film_work AS pfw ON p.id = pfw.person_id
-          WHERE pfw.film_work_id = fw.id AND pfw.role = 'AT') AS actors_names,
-    ARRAY(SELECT p.full_name FROM content.person AS p
-          JOIN content.person_film_work AS pfw ON p.id = pfw.person_id
-          WHERE pfw.film_work_id = fw.id AND pfw.role = 'SW') AS writers_names
+    array_agg(DISTINCT g.name) AS genres,
+    jsonb_agg(DISTINCT jsonb_build_object(
+        'id', p.id,
+        'full_name', p.full_name,
+        'role', pfw.role
+    )) AS person_details
 FROM 
     content.film_work AS fw
+LEFT JOIN 
+    content.person_film_work pfw ON pfw.film_work_id = fw.id
+LEFT JOIN 
+    content.person p ON pfw.person_id = p.id
+LEFT JOIN 
+    content.genre_film_work gfw ON gfw.film_work_id = fw.id
+LEFT JOIN 
+    content.genre g ON gfw.genre_id = g.id
 WHERE 
-    fw.id = ANY(%s);
+    fw.id = ANY(%s)
+GROUP BY 
+    fw.id
+ORDER BY 
+    fw.modified;
 """)
 
-get_modify_genres_query = sql.SQL("""
-SELECT 
-    id, modified
-    FROM content.genre
-    WHERE modified > %s
-    ORDER BY modified;
-""")
-
-get_modify_person_query = sql.SQL("""
-SELECT 
-    id, modified
-    FROM content.person
-    WHERE modified > %s
-    ORDER BY modified;
-""")
-
+# Запрос для получения всех ID фильмов, измененных с момента последней проверки
 get_all_modify_ids_query = sql.SQL("""
-    SELECT fw.id 
-    FROM content.film_work fw
-    LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
-    LEFT JOIN content.person p ON pfw.person_id = p.id
-    LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
-    LEFT JOIN content.genre g ON gfw.genre_id = g.id
-    WHERE p.id = ANY(%s)
-      OR g.id = ANY(%s)
-      OR fw.modified > %s
-    GROUP BY fw.id;
+SELECT fw.id 
+FROM content.film_work fw
+LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+LEFT JOIN content.person p ON pfw.person_id = p.id
+LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+LEFT JOIN content.genre g ON gfw.genre_id = g.id
+WHERE fw.modified > %s
+   OR p.modified > %s
+   OR g.modified > %s
+GROUP BY fw.id;
 """)
