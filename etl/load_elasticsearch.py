d81f38084628f66e879f5afe4c397f00da02f5ad
suvorovjr <pglavan1998@mail.ru>
2024-11-05 13:59:53 +0300

    Add exists index checker in project

--- etl/load_elasticsearch.py
+++ etl/load_elasticsearch.py
@@ -31,6 +31,30 @@ def create_bulk_action(film_work: FilmWork) -> dict:
     }
 
 
+@on_exception(expo, exceptions.ConnectionError, max_tries=10, max_time=100, logger=app_logger)
+def check_exists_index(es: Elasticsearch):
+    """
+    Проверяет существование индекса, если его нет, то попытается создать его
+    :param es: экземпляр соединения с Elasticsearch
+    Raises:
+        exceptions.ConnectionError: Если возникла ошибка соединения при создании индекса.
+        exceptions.BadRequestError: Если произошла ошибка с неправильным запросом при создании индекса.
+    """
+    index_name = settings.ELASTICSEARCH_INDEX_NAME
+
+    if not es.indices.exists(index=index_name):
+        app_logger.info(f'Индекса ElasticSearch с именем {index_name} не существует')
+        try:
+            es.indices.create(index=index_name, body=settings.ELASTICSEARCH_SCHEMA)
+            app_logger.info()
+        except exceptions.ConnectionError as e:
+            app_logger.error(f'Ошибка соединения при создании индекса "{index_name}": {e}')
+            raise
+        except exceptions.BadRequestError as e:
+            app_logger.error(f'Неправильный запрос при создании индекса "{index_name}": {e}')
+            raise
+
+
 def load_film_works_to_elasticsearch(film_works: List[FilmWork]) -> None:
     """Загружает список объектов FilmWork в Elasticsearch."""
     es = get_elasticsearch_client()
--- etl/settings.py
+++ etl/settings.py
@@ -47,14 +47,134 @@ DSL = {
     'options': '-c search_path=content',
 }
 
-ELASTICSEARCH_URL = get_env_variable('ELASTICSEARCH_URL', required=False)
-ELASTICSEARCH_INDEX_NAME = get_env_variable(
-    'ELASTICSEARCH_INDEX_NAME', required=False
-)
-
 REDIS_HOST = get_env_variable(
     'REDIS_HOST', default='localhost', required=False
 )
 REDIS_PASSWORD = get_env_variable('REDIS_PASSWORD', required=False)
 REDIS_PORT = int(get_env_variable('REDIS_PORT', default=6379, required=False))
 REDIS_DB = int(get_env_variable('REDIS_DB', default=0, required=False))
+
+ELASTICSEARCH_URL = get_env_variable('ELASTICSEARCH_URL', required=False)
+ELASTICSEARCH_INDEX_NAME = get_env_variable(
+    'ELASTICSEARCH_INDEX_NAME', required=False
+)
+ELASTICSEARCH_SCHEMA = {
+    "settings": {
+        "refresh_interval": "1s",
+        "analysis": {
+            "filter": {
+                "english_stop": {
+                    "type": "stop",
+                    "stopwords": "_english_"
+                },
+                "english_stemmer": {
+                    "type": "stemmer",
+                    "language": "english"
+                },
+                "english_possessive_stemmer": {
+                    "type": "stemmer",
+                    "language": "possessive_english"
+                },
+                "russian_stop": {
+                    "type": "stop",
+                    "stopwords": "_russian_"
+                },
+                "russian_stemmer": {
+                    "type": "stemmer",
+                    "language": "russian"
+                }
+            },
+            "analyzer": {
+                "ru_en": {
+                    "tokenizer": "standard",
+                    "filter": [
+                        "lowercase",
+                        "english_stop",
+                        "english_stemmer",
+                        "english_possessive_stemmer",
+                        "russian_stop",
+                        "russian_stemmer"
+                    ]
+                }
+            }
+        }
+    },
+    "mappings": {
+        "dynamic": "strict",
+        "properties": {
+            "id": {
+                "type": "keyword"
+            },
+            "imdb_rating": {
+                "type": "float"
+            },
+            "genres": {
+                "type": "keyword"
+            },
+            "title": {
+                "type": "text",
+                "analyzer": "ru_en",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
+                    }
+                }
+            },
+            "description": {
+                "type": "text",
+                "analyzer": "ru_en"
+            },
+            "directors_names": {
+                "type": "text",
+                "analyzer": "ru_en"
+            },
+            "actors_names": {
+                "type": "text",
+                "analyzer": "ru_en"
+            },
+            "writers_names": {
+                "type": "text",
+                "analyzer": "ru_en"
+            },
+            "directors": {
+                "type": "nested",
+                "dynamic": "strict",
+                "properties": {
+                    "id": {
+                        "type": "keyword"
+                    },
+                    "name": {
+                        "type": "text",
+                        "analyzer": "ru_en"
+                    }
+                }
+            },
+            "actors": {
+                "type": "nested",
+                "dynamic": "strict",
+                "properties": {
+                    "id": {
+                        "type": "keyword"
+                    },
+                    "name": {
+                        "type": "text",
+                        "analyzer": "ru_en"
+                    }
+                }
+            },
+            "writers": {
+                "type": "nested",
+                "dynamic": "strict",
+                "properties": {
+                    "id": {
+                        "type": "keyword"
+                    },
+                    "name": {
+                        "type": "text",
+                        "analyzer": "ru_en"
+                    }
+                }
+            }
+        }
+    }
+}
